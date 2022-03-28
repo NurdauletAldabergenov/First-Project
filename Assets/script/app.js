60b7ee9c2d11d55fdd7f41c1a02d40ef63772ebb
Nurdaulet <nurdaulet72@list.ru>
2022-03-28 18:14:51 +0600

    first commit

---
+++ Assets/script/app.js
@@ -0,0 +1,10 @@
+'use strict '
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?' , '')
+// const personalMvieDB = {
+// count: numberOfFilms,
+// muvies: {},
+// actors: {},
+// genres: [], 
+// privat: false
+// };
+// document.write(numberOfFilms)
